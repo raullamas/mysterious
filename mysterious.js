4b9c7b63caea5439192440e86d1982d2d6588d18
Raul Lamas <raul.lamas.b@me.com>
2021-05-07 14:00:20 -0500

    Use more descriptive names. Tweak findMostRelated()
    Use more descriptive names for variables and methods, to improve code readability. Adjust findMostRelated() a bit to make it more intuitive (update indexes on inner slice() and loop). Update to-do accordingly.

--- mysterious.js
+++ mysterious.js
@@ -1,37 +1,102 @@
-var onRecord = getOnRecord();
+var settings = getSettings(undefined, 15);
 var errors = getErrors();
-var projectMethods = coreMethods();
+var protoSpecim = getProtoSpecim();
 
-let manySturdy = getNRandSturdy(5, 100);
-console.log(manySturdy);
-console.log(onRecord);
+console.log(settings);
 
 //
-function coreMethods() {
+
+function findMostRelated (specims) {
+    if (!Array.isArray(specims) || specims.length < 2){
+        return 'Error: ' + errors.isNotArrayTwoEl;
+    } else if (specims.some(isInvalidSpecim)) {
+        return 'Error: ' + errors.invalidSpecims;
+    }
+    return specims.reduce(compareToAllOther, {'top relatedness': 0, IDs: [], strands: []});
+
+    function compareToAllOther (mostRelated, specim) {
+        let toCompareTo = specims.slice(specims.indexOf(specim) + 1);
+        for (let i = 0; i < toCompareTo.length; i++) {
+            let compared = specim.compare(toCompareTo[i])
+            if (compared.relatedness > mostRelated['top relatedness']) {
+                mostRelated.IDs = [compared.IDs];
+                mostRelated.strands = [compared.strands];
+                mostRelated['top relatedness'] = compared.relatedness;
+            } else if (compared.relatedness === mostRelated['top relatedness']) {
+                mostRelated.IDs.push(compared.IDs);
+                mostRelated.strands.push(compared.strands);
+            }
+        }
+        return mostRelated;
+    };
+}
+
+function getErrors () {
     return {
-        compareDna(specim) {
+        invalidID: `ID must be a numeric value between 0 and ${settings.poolSize - 1} (inclusive).`,
+        invalidStrand: `${settings.nucAcid === 'DNA' ? 'DNA' : 'RNA'} strand must be composed exclusively of bases ${settings.nucAcid === 'DNA' ? ['A', 'T', 'C', 'G'] : ['A', 'U', 'C', 'G']} and must be ${settings.strandLength} bases long.`,
+        poolIsFull: `There are already ${settings.poolSize} unique specimens on record. Update pool size to create new specimens.\n\n(Maximum number specimens is determined by raising number of nucleobases (4) to the power of strand length (${settings.strandLength}).)`,
+        duplicateID (id) {
+            return `ID ${id} is already on record. Choose a different ID.`;
+        },
+        duplicateStrand (strand) {
+            return `${settings.nucAcid === 'DNA' ? 'DNA' : 'RNA'} strand ${strand} is already on record. Choose a different strand.`;
+        },
+        invalidSpecim: 'Specimen to compare to must be created through command "newSpecim".',
+        invalidNumMutations: `Number of mutations must be a numeric value between 1 and ${settings.strandLength} (inclusive).`,
+        invalidSturdinessPcent: 'Sturdiness percentage must be a numeric value between 0 and 100 (inclusive).',
+        invalidNumSturdy () {
+            return `Number of specimens must be a numeric value between 1 and ${settings.poolSize - settings.IDs.size} (number of available specimens as per pool size), inclusive.`
+        },
+        isNotArrayTwoEl: 'Specimens to evaluate must be passed inside an array of at least 2 elements.',
+        invalidSpecims: 'All specimens to evaluate must be created through command "newSpecim".'
+    }
+}
+
+function getNRandSturdy(num = 30, pcent = 60) {
+    if (typeof num !== 'number' || num < 1 || num > settings.poolSize - settings.IDs.size) {
+        return 'Error: ' + errors.invalidNumSturdy();
+    } else if (typeof pcent !== 'number' || pcent < 0 || pcent > 100) {
+        return 'Error: ' + errors.invalidSturdinessPcent;
+    }
+    var sturdy = [];
+    while (sturdy.length < num) {
+        let randSpecim = newSpecim();
+        if (randSpecim.isSturdy(pcent)) {
+            sturdy.push(randSpecim);
+        } else {
+            settings.IDs.delete(randSpecim.ID);
+            settings.strands.delete(randSpecim.strand);
+        }
+    }
+    return sturdy;
+}
+
+function getProtoSpecim() {
+    return {
+        compare(specim) {
             if (isInvalidSpecim(specim)) {
                 return 'Error: ' + errors.invalidSpecim;
             }
             let simil = 0;
-            for (let i = 0, j = 0; i < this.dnaSeq.length; i++, j++) {
-                if (this.dnaSeq[i] === specim.dnaSeq[j]) {
+            for (let i = 0, j = 0; i < this.strand.length; i++, j++) {
+                if (this.strand[i] === specim.strand[j]) {
                     simil++;
                 }
             };
             return { 
-                iDs: [this.specimId, specim.specimId],
-                dnaSeqs: [this.dnaSeq, specim.dnaSeq],
-                compatibility: +(simil / this.dnaSeq.length * 100).toFixed(2)
+                IDs: [this.ID, specim.ID],
+                strands: [this.strand, specim.strand],
+                relatedness: +(simil / this.strand.length * 100).toFixed(2)
             }
         },
-        complemDnaSeq() {
-            return Array.from(this.dnaSeq).map(complemDnaBase);
+        complement() {
+            return Array.from(this.strand).map(complemBase);
 
-            function complemDnaBase(base) {
+            function complemBase(base) {
                 switch (base) {
                     case 'A':
-                        return onRecord.nucAcid === 'DNA' ? 'T' : 'U';
+                        return settings.nucAcid === 'DNA' ? 'T' : 'U';
                     case 'T':
                     case 'U':
                         return 'A';
@@ -44,109 +109,43 @@ function coreMethods() {
                 }
             }
         },
-        mutate(num = 1) {
-            if (typeof num !== 'number' || num > this.dnaSeq.length || num < 1) {
+        mutate(num = 5) {
+            if (typeof num !== 'number' || num > settings.strandLength || num < 1) {
                 return 'Error: ' + errors.invalidNumMutations;
             }
-            let [mutatedSeq, numSwaps, idxsOfSwaps] = [this.dnaSeq, 0, new Set()];
+            let [newStrand, numSwaps, idxsOfSwaps] = [this.strand, 0, new Set()];
             while (numSwaps < num) {
-                let [swapIdx, swappedBase] = [Math.floor(Math.random() * mutatedSeq.length), randDnaBase()];
-                if (mutatedSeq[swapIdx] === swappedBase || idxsOfSwaps.has(swapIdx)) {
+                let [swapIdx, swappedBase] = [Math.floor(Math.random() * newStrand.length), randBase()];
+                if (newStrand[swapIdx] === swappedBase || idxsOfSwaps.has(swapIdx)) {
                     continue;
                 }
-                mutatedSeq = swapOneChar(mutatedSeq, swapIdx, swappedBase);
+                newStrand = swapOneBase(newStrand, swapIdx, swappedBase);
                 numSwaps++;
                 idxsOfSwaps.add(swapIdx);
             }
-            return mutatedSeq;
+            return newStrand;
 
-            function swapOneChar(str, idx, replacement) {
-                return str.substring(0, idx) + replacement + str.substring(idx + 1);
+            function swapOneBase(strand, idx, replacement) {
+                return strand.substring(0, idx) + replacement + strand.substring(idx + 1);
             }
         }, 
         isSturdy(pcent = 60) {
             if (typeof pcent !== 'number' || pcent > 100 || pcent < 0) {
                 return 'Error: ' + errors.invalidSturdinessPcent;
             }
-            return Array.from(this.dnaSeq).reduce(reducer, 0) >= pcent/100 * onRecord.strandLength;
+            return Array.from(this.strand).reduce(incrementIf, 0) >= pcent/100 * settings.strandLength;
 
-            function reducer(acc, curVal) {
-                if (curVal === 'C' || curVal === 'G') {
+            function incrementIf(acc, base) {
+                if (base === 'C' || base === 'G') {
                     acc++;
                 }
                 return acc;
             }
         }
     }
-};
-
-function findMostRelated (specimens) {
-    if (!Array.isArray(specimens) || specimens.length < 2){
-        return 'Error: ' + errors.isNotArray;
-    } else if (specimens.some(isInvalidSpecim)) {
-        return 'Error: ' + errors.invalidSpecims;
-    }
-    return specimens.reduce(reducer1, {'top compatibility': 0, iDs: [], dnaSeqs: []});
-
-    function reducer1 (acc, curVal) {
-        let notYetCompared = specimens.slice(specimens.indexOf(curVal));
-        for (let i = 1; i < notYetCompared.length; i++) {
-            let compared = curVal.compareDna(notYetCompared[i])
-            if (compared.compatibility > acc['top compatibility']) {
-                acc.iDs = [compared.iDs];
-                acc.dnaSeqs = [compared.dnaSeqs];
-                acc['top compatibility'] = compared.compatibility;
-            } else if (compared.compatibility === acc['top compatibility']) {
-                acc.iDs.push(compared.iDs);
-                acc.dnaSeqs.push(compared.dnaSeqs);
-            }
-        }
-        return acc;
-    };
 }
 
-function getErrors () {
-    return {
-        invalidID: `Specimen ID must be a numeric value between 0 and ${onRecord.poolSize - 1} (inclusive).`,
-        invalidDnaSeq: `Strand must be composed exclusively of bases ${onRecord.nucAcid === 'DNA' ? ['A', 'T', 'C', 'G'] : ['A', 'U', 'C', 'G']} and must be ${onRecord.strandLength} bases long.`,
-        poolIsFull: `There are already ${onRecord.poolSize} unique specimens on record. Update pool size to create new specimens.\n\n(Maximum number specimens is determined by raising number of nucleobases (4) to the power of user-defined DNA sequence length (${onRecord.strandLength}).)`,
-        duplicateID (id) {
-            return `Specim ID ${id} is already on record. Choose a different ID.`;
-        },
-        duplicateDNASeq (dnaSeq) {
-            return `DNA sequence ${dnaSeq} is already on record. Choose a different DNA sequence.`;
-        },
-        invalidSpecim: 'Specimen to compare to must be created through command "newSpecim".',
-        invalidNumMutations: `Number of mutations must be a numeric value between 1 and ${onRecord.strandLength} (inclusive).`,
-        invalidSturdinessPcent: 'Sturdiness percentage must be a numeric value between 0 and 100 (inclusive).',
-        invalidNumRandSturdy () {
-            return `Number of specimens must be a numeric value between 1 and ${onRecord.poolSize - onRecord.iDs.size} (number of available specimens as per pool size), inclusive.`
-        },
-        isNotArray: 'Specimens to evaluate must be passed as an array of at least 2 elements.',
-        invalidSpecims: 'All specimens to evaluate must be created through command "newSpecim".'
-    }
-}
-
-function getNRandSturdy(num, pcent = 60) {
-    if (typeof num !== 'number' || num < 1 || num > onRecord.poolSize - onRecord.iDs.size) {
-        return 'Error: ' + errors.invalidNumRandSturdy();
-    } else if (typeof pcent !== 'number' || pcent < 0 || pcent > 100) {
-        return 'Error: ' + errors.invalidSturdinessPcent;
-    }
-    var sturdy = [];
-    while (sturdy.length < num) {
-        let randSpecim = newSpecim();
-        if (randSpecim.isSturdy(pcent)) {
-            sturdy.push(randSpecim);
-        } else {
-            onRecord.iDs.delete(randSpecim.specimId);
-            onRecord.dnaSeqs.delete(randSpecim.dnaSeq);
-        }
-    }
-    return sturdy;
-}
-
-function getOnRecord (nucAcid = 'DNA', strandLength = 15) {
+function getSettings (nucAcid = 'DNA', strandLength = 15) {
     if (nucAcid !== 'DNA' && nucAcid !== 'RNA') {
         return 'Error: ' + 'Nucleic acid must be either \'DNA\' or \'RNA\'.';
     } else if (typeof strandLength !== 'number' || strandLength > 15 || strandLength < 2) {
@@ -161,64 +160,64 @@ function getOnRecord (nucAcid = 'DNA', strandLength = 15) {
         get poolSize () {
             return Math.pow(4, this.strandLength);
         },
-        iDs: new Set(),
-        dnaSeqs: new Set(),
+        IDs: new Set(),
+        strands: new Set(),
     }
 }
 
 function isInvalidSpecim (specim) {
-    return Object.getPrototypeOf(Object(specim)) !== projectMethods;
+    return Object.getPrototypeOf(Object(specim)) !== protoSpecim;
 }
 
-function newSpecim(specimId = randSpecimId(), dnaSeq = randDnaSeq()) {
-    if (typeof specimId !== 'number' || specimId >= onRecord.poolSize || specimId < 0) {
+function newSpecim(ID = randID(), strand = randStrand()) {
+    if (typeof ID !== 'number' || ID >= settings.poolSize || ID < 0) {
         return 'Error: ' + errors.invalidID;
-    } else if (typeof dnaSeq !== 'string' || dnaSeq.length !== onRecord.strandLength || isInvalidSeq(dnaSeq)) {
-        return 'Error: ' + errors.invalidDnaSeq;
+    } else if (typeof strand !== 'string' || strand.length !== settings.strandLength || isInvalidStrand(strand)) {
+        return 'Error: ' + errors.invalidStrand;
     };
-    if (onRecord.iDs.size >= onRecord.poolSize || onRecord.dnaSeqs.size >= onRecord.poolSize) {
+    if (settings.IDs.size >= settings.poolSize || settings.strands.size >= settings.poolSize) {
         return 'Error: ' + errors.poolIsFull;
     };
-    if (onRecord.iDs.has(specimId)) {
-        return 'Error: ' + errors.duplicateID(specimId);
-    } else if (onRecord.dnaSeqs.has(dnaSeq)) {
-        return 'Error: ' + errors.duplicateDNASeq(dnaSeq);
+    if (settings.IDs.has(ID)) {
+        return 'Error: ' + errors.duplicateID(ID);
+    } else if (settings.strands.has(strand)) {
+        return 'Error: ' + errors.duplicateStrand(strand);
     };
-    onRecord.iDs.add(specimId), onRecord.dnaSeqs.add(dnaSeq);
-    let specimen = Object.create(projectMethods);
-    specimen.specimId = specimId, specimen.dnaSeq = dnaSeq;
-    return specimen;
+    settings.IDs.add(ID), settings.strands.add(strand);
+    let specim = Object.create(protoSpecim);
+    specim.ID = ID, specim.strand = strand;
+    return specim;
 
-    function isInvalidSeq(str) {
-        let arr = Array.from(str);
-        return !arr.every(char => onRecord.bases.includes(char));
+    function isInvalidStrand(strand) {
+        let strandArr = Array.from(strand);
+        return !strandArr.every(base => settings.bases.includes(base));
     }
 }
 
-function randDnaSeq() {
-    if (onRecord.dnaSeqs.size < onRecord.poolSize) {
+function randBase() {
+    const bases = settings.bases;
+    return bases[Math.floor(Math.random() * 4)];
+}
+
+function randID() {
+    if (settings.IDs.size < settings.poolSize) {
+        let ID = Math.floor(Math.random() * settings.poolSize);
+        if (settings.IDs.has(ID)) {
+            return randID();
+        };
+        return ID;
+    }
+}
+
+function randStrand() {
+    if (settings.strands.size < settings.poolSize) {
         let newStrand = '';
-        for (let i = 0; i < onRecord.strandLength; i++) {
-            newStrand += randDnaBase();
+        for (let i = 0; i < settings.strandLength; i++) {
+            newStrand += randBase();
         }
-        if (onRecord.dnaSeqs.has(newStrand)) {
-            return randDnaSeq();
+        if (settings.strands.has(newStrand)) {
+            return randStrand();
         };
         return newStrand;
     }
-}
-
-function randSpecimId() {
-    if (onRecord.iDs.size < onRecord.poolSize) {
-        let specimId = Math.floor(Math.random() * onRecord.poolSize);
-        if (onRecord.iDs.has(specimId)) {
-            return randSpecimId();
-        };
-        return specimId;
-    }
-}
-
-function randDnaBase() {
-    const bases = onRecord.bases;
-    return bases[Math.floor(Math.random() * bases.length)];
 }
